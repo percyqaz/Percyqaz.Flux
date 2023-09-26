502ace13e1b9fac30c37bc60830d2b6942a40539
Autumn Leaf <kurulen@icloud.com>
2023-09-26 15:20:23 -0500

    Fix for windowing on macOS

--- src/Windowing/Window.fs
+++ src/Windowing/Window.fs
@@ -43,7 +43,7 @@ module Window =
 
 [<Sealed>]
 type Window(config: Config, title: string, root: Root) as this =
-    inherit NativeWindow(NativeWindowSettings(StartVisible = false, NumberOfSamples = 24))
+    inherit NativeWindow(NativeWindowSettings(StartVisible = false, NumberOfSamples = 24, Flags = ContextFlags.ForwardCompatible, Profile = ContextProfile.Core)
 
     let renderThread = RenderThread(this, config.AudioDevice.Value, root, WindowEvents.afterInit.Trigger)
 
@@ -53,7 +53,7 @@ type Window(config: Config, title: string, root: Root) as this =
     do
         base.Title <- title
         base.VSync <- VSyncMode.Off
-        base.CursorState <- CursorState.Hidden
+        base.CursorState <- CursorState.Grabbed
 
     member this.ApplyConfig(config: Config) =
 
